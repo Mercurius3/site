5494150899a36b9041b19fa414d91027bf6bc8ed
Axel Graff <axelgraff@gmail.com>
2013-10-29 21:19:58 +0100

    added pictures to pages

--- app/models/page.rb
+++ app/models/page.rb
@@ -2,4 +2,5 @@ class Page < ActiveRecord::Base
   rails_admin do
     
   end
+  mount_uploader :picture, PictureUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131029081558) do
+ActiveRecord::Schema.define(version: 20131029195158) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20131029081558) do
     t.integer  "position"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "picture"
   end
 
   create_table "posts", force: true do |t|
