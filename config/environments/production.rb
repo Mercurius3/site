8db7e214a930975f1ae53533abba2c6c73e43a79
Axel Graff <axelgraff@gmail.com>
2013-11-08 20:42:49 +0100

    added staging default host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Site::Application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   # config.action_mailer.delivery_method = :sendmail
-  # config.action_mailer.default_url_options = { :host => "staging.lassche-lassche.nl" }
+  config.action_mailer.default_url_options = { :host => "staging.lassche-lassche.nl" }
   
   config.action_mailer.smtp_settings = {
     :address   => "smtp.mandrillapp.com",
