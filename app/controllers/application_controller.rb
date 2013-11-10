751d91722a6687f049ed667f282431fcce8671e2
Axel Graff <axelgraff@gmail.com>
2013-11-10 11:43:13 +0100

    fixed parameter sanitizer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,18 +17,18 @@ class ApplicationController < ActionController::Base
   end
   
   protected
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :first_name
-    devise_parameter_sanitizer.for(:account_update) << :first_name
-  end
-  
-  # def devise_parameter_sanitizer
-  #   if resource_class == User
-  #      User::ParameterSanitizer.new(User, :user, params)
-  #   else
-  #      super
-  #   end
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.for(:sign_up) << :first_name
+  #   devise_parameter_sanitizer.for(:account_update) << :first_name
   # end
+  
+  def devise_parameter_sanitizer
+    if resource_class == User
+       User::ParameterSanitizer.new(User, :user, params)
+    else
+       super
+    end
+  end
   
   private
   def set_nav_var
