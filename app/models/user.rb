c51de9f0fbd72c9517535bfdc8f26e06b37cfa51
Axel Graff <axelgraff@gmail.com>
2013-11-11 23:38:25 +0100

    added signup for newsletter through form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  before_create :subscription_checked?
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
@@ -7,5 +8,20 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable, :registerable, :confirmable
   has_many :transactions
   
-  validate :email, uniqueness: true, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create } 
+  validate :email, uniqueness: true, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create }
+  
+  private
+  def subscription_checked?
+    if self.news_letter
+      @gb = Gibbon::API.new(ENV['MAILCHIMP_API_TOKEN'])
+      @gb.lists.subscribe({
+        :id => ENV['MAILCHIMP_LIST_ID'], 
+        :email => {:email => self.email},
+        :merge_vars => {
+          :FNAME => self.first_name,
+          :LNAME => self.last_name},
+        :double_optin => false
+      })
+    end
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,5 +1,7 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-
+  test "User aanmaken lukt" do
+    User.create!(first_name: users(:one).first_name, last_name: users(:one).last_name)
+  end
 end
