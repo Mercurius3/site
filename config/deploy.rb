ae58905d9f4f59dada82bd82eb20b9cbbcc6ffae
Axel Graff <axelgraff@gmail.com>
2013-12-31 00:06:39 +0100

    ssh

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,12 +5,12 @@ set :branch, "master"
 
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
-# set :deploy_to, '/var/www/vhosts/lassche-lassche.nl/staging'
-# set :scm, :git
+set :deploy_to, '/var/www/vhosts/lassche-lassche.nl/staging'
+set :scm, :git
 
-# set :format, :pretty
+set :format, :pretty
 # set :log_level, :debug
-# set :pty, true
+set :pty, true
 
 # set :linked_files, %w{config/database.yml}
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -4,10 +4,10 @@ set :stage, :staging
 # ==================
 # Supports bulk-adding hosts to roles, the primary
 # server in each group is considered to be the first
-# unless any hosts have the primary property set.
-# role :app, %w{mushu.bluerail.nl}
-# role :web, %w{mushu.bluerail.nl}
-# role :db,  %w{mushu.bluerail.nl}, primary: true
+# unless any hosts have the primary property set.# 
+role :app, 'mushu.bluerail.nl'
+role :web, 'mushu.bluerail.nl'
+role :db,  'mushu.bluerail.nl', primary: true
 
 # Extended Server Syntax
 # ======================
@@ -15,29 +15,32 @@ set :stage, :staging
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server 'mushu.bluerail.nl', user: 'lassche', roles: %w{web app db}
+
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
 # you can see them in [net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start)
 # set it globally
-#  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
+ set :ssh_options, {
+   keys: %w(/Users/axel/.ssh/id_rsa),
+   forward_agent: true,
+   port: 4321,
+   auth_methods: %w(publickey)
+ }
+
 # and/or per server
-# server 'example.com',
-#   user: 'user_name',
-#   roles: %w{web app},
-#   ssh_options: {
-#     user: 'user_name', # overrides user setting above
-#     keys: %w(/home/user_name/.ssh/id_rsa),
-#     forward_agent: false,
-#     auth_methods: %w(publickey password)
-#     # password: 'please use keys'
-#   }
+ # server 'mushu.bluerail.nl',
+ #  user: 'lassche',
+ #  roles: %w{web app},
+ #  ssh_options: {
+ #    user: 'lassche', # overrides user setting above
+ #    keys: %w(/Users/axel/.ssh/id_rsa),
+ #    forward_agent: false,
+ #    auth_methods: %w(publickey password)
+ #    # password: 'please use keys'
+ #  }
 # setting per server overrides global ssh_options
 set :deploy_to, "var/www/vhosts/lassche-lassche.nl/staging"
+set :tmp_dir, "var/www/vhosts/lassche-lassche.nl/staging/tmp"
 
 fetch(:default_env).merge!(rails_env: :production)
