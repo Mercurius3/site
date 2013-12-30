23ccf877df59d8abf50596f4d1358fdb389e660d
Axel Graff <axelgraff@gmail.com>
2013-12-31 00:24:50 +0100

    users

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -5,9 +5,9 @@ set :stage, :staging
 # Supports bulk-adding hosts to roles, the primary
 # server in each group is considered to be the first
 # unless any hosts have the primary property set.# 
-role :app, 'mushu.bluerail.nl'
-role :web, 'mushu.bluerail.nl'
-role :db,  'mushu.bluerail.nl', primary: true
+# role :app, 'mushu.bluerail.nl'
+# role :web, 'mushu.bluerail.nl'
+# role :db,  'mushu.bluerail.nl', primary: true
 
 # Extended Server Syntax
 # ======================
@@ -21,24 +21,27 @@ role :db,  'mushu.bluerail.nl', primary: true
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
 # you can see them in [net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start)
 # set it globally
- set :ssh_options, {
-   keys: %w(/Users/axel/.ssh/id_rsa),
-   forward_agent: true,
-   port: 4321,
-   auth_methods: %w(publickey)
- }
+ # set :ssh_options, {
+ #   keys: '/Users/axel/.ssh/id_rsa',
+ #   user: 'lassche',
+ #   auth_methods: %w(publickey),
+ #   forward_agent: true,
+ #   verbose: :debug,
+ #   # auth_methods: %w(publickey)
+ # }
 
 # and/or per server
- # server 'mushu.bluerail.nl',
- #  user: 'lassche',
- #  roles: %w{web app},
- #  ssh_options: {
- #    user: 'lassche', # overrides user setting above
- #    keys: %w(/Users/axel/.ssh/id_rsa),
- #    forward_agent: false,
- #    auth_methods: %w(publickey password)
- #    # password: 'please use keys'
- #  }
+ server 'lassche@mushu.bluerail.nl',
+  user: 'lassche',
+  roles: %w{web app db},
+  ssh_options: {
+    user: 'lassche', # overrides user setting above
+    keys: %w(/Users/axel/.ssh/id_rsa),
+    forward_agent: false,
+    verbose: :debug,
+    auth_methods: %w(publickey password)
+    # password: 'please use keys'
+  }
 # setting per server overrides global ssh_options
 set :deploy_to, "var/www/vhosts/lassche-lassche.nl/staging"
 set :tmp_dir, "var/www/vhosts/lassche-lassche.nl/staging/tmp"
