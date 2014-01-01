543351eba419ae997a5d75a593f7900710b3114a
Axel Graff <axelgraff@gmail.com>
2014-01-01 14:51:01 +0100

    removed version normal

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -33,9 +33,9 @@ class PictureUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  version :normal do
-    process :scale => [640, 480]
-  end
+  # version :normal do
+  #   process :scale => [640, 480]
+  # end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
