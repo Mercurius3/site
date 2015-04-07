b9459a28eabfa66b04d63011399b6c2d1e9cee83
Mercurius3 <axelgraff@gmail.com>
2015-04-07 12:24:30 +0200

    added new fonts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,6 +21,7 @@
 //= require jquery.validate.localization/messages_nl
 //= require users
 //= require youtube
+//= require MyFontsWebfontsKit
 $(document).foundation();
 if (!(/^http:\/\/lassche-lassche.nl/).test(window.location.href)) {
 	$('<header id="devbanner">').prependTo('body');
