a4bb813798bd0ce7439b57af59b5393b1bfdbea2
Mercurius3 <axelgraff@gmail.com>
2015-04-15 14:29:59 +0200

    renaming config.serve_static_files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Site::Application.configure do
   config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_files = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -76,8 +76,8 @@ Site::Application.configure do
   # config.autoflush_log = false
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
-  config.log_formatter = ::Logger::Formatter.new
-	#
+	config.log_formatter = ::Logger::Formatter.new
+	
 	# Test compression
 	config.middleware.use Rack::Deflater
 	config.middleware.use HtmlCompressor::Rack, {
