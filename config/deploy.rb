12319bb3df26201a53409a88d5549042d9d4ad08
Mercurius3 <axelgraff@gmail.com>
2015-05-15 09:46:25 +0200

    testing shared files capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,9 +24,10 @@ set :repo_url, 'git@github.com:Mercurius3/site.git'
 
 # Default value for :linked_files is []
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'app_environment_variables.rb')
 
 # Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
