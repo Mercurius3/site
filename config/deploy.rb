783e27fd4e2bf9768a69a56b5730f3cb3f09bf37
Mercurius3 <axelgraff@gmail.com>
2015-05-18 13:00:42 +0200

    check staging environment

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,7 +31,7 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
-set :rails_env, "production"
+# set :rails_env, "production"
 
 # Default value for keep_releases is 5
 set :keep_releases, 3
