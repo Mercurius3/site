ed4abbb07c1a92cb73280ea87354c29bd121fac5
Mercurius3 <axelgraff@gmail.com>
2015-05-25 13:29:23 +0200

    adding image to mailer

--- app/mailers/transaction_mailer.rb
+++ app/mailers/transaction_mailer.rb
@@ -3,7 +3,7 @@ class TransactionMailer < ActionMailer::Base
 
     def transaction_confirmation(transaction)
       @transaction = transaction
-			attachments.inline['logo.png'] = File.read('logo.png')
+			attachments.inline['logo.png'] = File.read(image_path('logo.png'))
       mail(to: @transaction.user.email, bcc: ["mercurius3@me.com","marielle@lassche-lassche.nl"], subject: "Aanmelding #{@transaction.product.name}")
     end
 end
